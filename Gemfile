150cf8c987e7b96843e593c3585f717ab84cf9b1
Colton <colton@coltonseal.com>
2014-03-28 14:36:14 -0500

    Finished user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,11 +6,14 @@ gem 'rails', '4.0.2'
 gem 'pg', '0.15.1'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :development do
 	#gem 'sqlite3', '1.3.8'
 	gem 'rspec-rails', '2.13.1'
-end 
+end
 
 group :test do
 	gem 'selenium-webdriver', '2.35.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
-  def new
+
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy #this ensures only amdins can delete users
+
+def new
   	@user = User.new
+end
+
+def destroy
+  User.find(params[:id]) .destroy
+  flash[:success] = "User Deleted"
+  redirect_to users_url
+end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -18,10 +33,38 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+  end
+
   private
 
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  		
   	end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please Sign In."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user #this ensures only amdins can delete users
+        redirect_to(root_url) unless current_user.admin?
+    end
+
 end
